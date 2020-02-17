ba871038cd2540c8f34f0e69a833946ee7460b32
bachdx <bachdx.hut@gmail.com>
2020-02-17 17:32:30 +0700

    paging & per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  include Pagy::Backend
+
   def index
-    @users = User.all
+    @pagy, @users = pagy(User.all, items: params[:per_page] || 20)
 
     respond_to do |format|
       format.html
       format.json do
         render json: @users,
-               each_serializer: UserSerializer
+               adapter: :json,
+               each_serializer: UserSerializer,
+               meta: {
+                 total: @pagy.count,
+                 page: @pagy.page,
+                 from: @pagy.from,
+                 to: @pagy.to,
+                 series: @pagy.series
+               }
       end
     end
   end
--- app/javascript/packs/pages/users/index.vue
+++ app/javascript/packs/pages/users/index.vue
@@ -1,42 +1,85 @@
 <template>
-  <table class="table">
-    <thead id="test">
-      <tr>
-        <th scope="col">#ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Description</th>
-        <th scope="col">Gender</th>
-        <th scope="col">School</th>
-      </tr>
-    </thead>
-    <tbody>
-      <UserItem v-for="user in usersList" :key="user.id" :user="user"></UserItem>
-    </tbody>
-  </table>
+  <div>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect01">Per Page:</label>
+      </div>
+      <select v-model="perPage">
+        <option v-for="option in perPageOptions" :key="option">{{option}}</option>
+      </select>
+    </div>
+
+    <Paging :meta="meta" v-on:change-page="pageChanged"></Paging>
+    <table class="table">
+      <thead id="test">
+        <tr>
+          <th scope="col">#ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Gender</th>
+          <th scope="col">School</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- 20 items per page -->
+        <UserItem v-for="user in usersList" :key="user.id" :user="user"></UserItem>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import UserItem from "../../components/Users/UserItem";
 
+const perPageOptions = [10, 20, 30, 40, 50, 60];
+
 export default {
   components: {
-    UserItem
+    UserItem,
+    Paging: () => import("../../components/Share/Paging")
   },
   data: function() {
     return {
-      usersList: []
+      usersList: [],
+      page: 1,
+      perPage: 20,
+      meta: {}
     };
   },
   created: function() {
+    this.initConstants();
     this.fetchUsersList();
   },
   mounted: function() {},
   methods: {
+    initConstants: function() {
+      this.perPageOptions = perPageOptions;
+      // so on
+    },
     fetchUsersList: async function() {
-      const result = await this.axios.get("users.json");
+      let params = {
+        page: this.page,
+        per_page: this.perPage
+      };
+      const result = await this.axios.get("users.json", {
+        params: params
+      });
 
-      console.log(result);
-      this.usersList = result.data;
+      this.usersList = result.data.users;
+      // this.meta = result.data.meta;
+
+      this.meta = Object.assign({}, this.meta, result.data.meta);
+      this.page = result.data.meta.page;
+    },
+    pageChanged: function(page) {
+      this.page = page;
+      this.fetchUsersList();
+    }
+  },
+  watch: {
+    perPage: function() {
+      this.page = 1;
+      this.fetchUsersList();
     }
   }
 };
